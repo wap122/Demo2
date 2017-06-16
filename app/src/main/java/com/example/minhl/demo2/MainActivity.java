2678c743acaa8d52a4574a3fb0564402105746fd
MinhSacred <minhlevan104@gmail.com>
2017-06-17 01:44:08 +0700

    Update 1

--- app/src/main/java/com/example/minhl/demo2/MainActivity.java
+++ app/src/main/java/com/example/minhl/demo2/MainActivity.java
@@ -9,33 +9,47 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnResult;
+    private Button btnResult, btnDevided;
     private EditText edtNumberA, edtNumberB;
-    private int a , b;
+    private int a, b;
     private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnResult= (Button) findViewById(R.id.btn_result);
+        btnResult = (Button) findViewById(R.id.btn_result);
+        btnDevided = (Button) findViewById(R.id.btn_devided);
         edtNumberA = (EditText) findViewById(R.id.edt_a);
-        edtNumberB= (EditText) findViewById(R.id.edt_b);
+        edtNumberB = (EditText) findViewById(R.id.edt_b);
 
         btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!edtNumberA.getText().toString().equals("") && !edtNumberB.getText().toString().equals("")){
+                if (!edtNumberA.getText().toString().equals("") && !edtNumberB.getText().toString().equals("")) {
                     a = Integer.parseInt(edtNumberA.getText().toString());
                     b = Integer.parseInt(edtNumberB.getText().toString());
-                    result = String.valueOf(a*b);
-                    Toast.makeText(MainActivity.this, "Kết quả của "+a+"+"+b+" là:" + result, Toast.LENGTH_LONG).show();
-                }else{
+                    result = String.valueOf(a * b);
+                    Toast.makeText(MainActivity.this, "Kết quả của " + a + " * " + b + " là: " + result, Toast.LENGTH_LONG).show();
+                } else {
                     Toast.makeText(MainActivity.this, "Dữ liệu nhập vào không hợp ", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+        btnDevided.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!edtNumberA.getText().toString().equals("") && !edtNumberB.getText().toString().equals("")) {
+                    a = Integer.parseInt(edtNumberA.getText().toString());
+                    b = Integer.parseInt(edtNumberB.getText().toString());
+                    result = String.valueOf(a / b);
+                    Toast.makeText(MainActivity.this, "Kết quả của " + a + " / " + b + " là: " + result, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Dữ liệu nhập vào không hợp ", Toast.LENGTH_SHORT).show();
+                }
 
+            }
+        });
     }
 }
